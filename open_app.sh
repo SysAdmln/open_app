f96d767d71b842df43a1a687430a2d3d52f9f544
SysAdmin <sys@dmin.pro>
2022-02-07 12:16:38 +0300

    add beauty

--- open_app.sh
+++ open_app.sh
@@ -1,1 +1,1 @@
-if test $# -gt 0; then mdfind $1 "kind:app" | grep -v '/Users/' | awk '{print "\x27" $0 "\x27" }' | fzf -1 -0| xargs open; fi
+if test $# -gt 0; then mdfind $1 "kind:app" | grep -v '/Users/' | awk '{print "\x27" $0 "\x27" }' | fzf -1 -0 --cycle --height=10| xargs open; fi
