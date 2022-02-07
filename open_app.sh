9cb5460eb78c8e2cf5e09de576dce65325d8b916
SysAdmin <sys@dmin.pro>
2022-02-07 14:32:05 +0300

    fix

--- open_app.sh
+++ open_app.sh
@@ -1,1 +1,3 @@
-if test $# -gt 0; then mdfind $1 "kind:app" | grep -v '/Users/' | awk '{print "\x27" $0 "\x27" }' | fzf -1 -0 --cycle --height=10| xargs open; fi
+if test $# -gt 0; #проверка на значение, иначе все программы
+  then mdfind $1 "kind:app" | grep -v '/Users/' | awk '{print "\x27" $0 "\x27" }' | fzf -1 -0 --cycle --height=10 | xargs open; 
+fi
